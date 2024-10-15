cb8623439fc1c645b3bb51eb659de9022664a249
Airat Galiamov <DuzaBF@users.noreply.github.com>
2024-10-15 21:14:49 +0200

    Implement load and store access fault

--- sim/tests/null_deref/null_deref.c
+++ sim/tests/null_deref/null_deref.c
@@ -1,13 +1,25 @@
 #include "stdint.h"
 #include "stddef.h"
 
+// #define STORE
+
 void simple() {
     volatile uint32_t a = *(volatile uint32_t*) NULL;
 }
 
+#ifdef STORE
+void foo(volatile uint32_t* p) {
+     *p = 0xDEADF00D;
+}
+#else 
+void foo(volatile uint32_t* p) {
+    volatile uint32_t a = *p;
+}
+#endif
+
 int main()
 {
-    volatile uint32_t a = *(volatile  uint32_t*) NULL;
     simple();
+    foo(NULL);
     return 0;
 }
