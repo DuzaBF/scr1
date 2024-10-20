fd2671fc380194160fc23165d2fa06dcab88e237
Airat Galiamov <DuzaBF@users.noreply.github.com>
2024-10-20 16:59:01 +0200

    Add TDU break

--- sim/tests/null_deref/null_deref.c
+++ sim/tests/null_deref/null_deref.c
@@ -1,24 +1,34 @@
 #include "stdint.h"
 #include "stddef.h"
+#include "riscv_csr_encoding.h"
 
 // #define STORE
+// #define USE_TDU_BREAK
 
-void simple() {
-    volatile uint32_t a = *(volatile uint32_t*) NULL;
+void simple()
+{
+    volatile uint32_t a = *(volatile uint32_t *)NULL;
 }
 
 #ifdef STORE
-void foo(volatile uint32_t* p) {
-     *p = 0xDEADF00D;
+void foo(volatile uint32_t *p)
+{
+    *p = 0xDEADF00D;
 }
-#else 
-void foo(volatile uint32_t* p) {
+#else
+void foo(volatile uint32_t *p)
+{
     volatile uint32_t a = *p;
 }
 #endif
 
 int main()
 {
+#ifdef USE_TDU_BREAK
+    set_csr(mcontrol, MCONTROL_M | MCONTROL_EXECUTE |
+                          MCONTROL_STORE |
+                          MCONTROL_LOAD);
+#endif
     simple();
     foo(NULL);
     return 0;
